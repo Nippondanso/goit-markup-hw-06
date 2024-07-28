2aa0a36e9793d91da937ed8b59314f877d91cb92
Maksym Kopaiev <baton-opuses-0e@icloud.com>
2024-07-28 04:16:26 +0300

    almost done (wait for advanced js)

---
+++ js/menu.js
@@ -0,0 +1,26 @@
+
+(() => {
+    const refs = {
+        openmenuBtn: document.querySelector('[menu-btn-open]'),
+        closemenuBtn: document.querySelector('[menu-btn-close]'),
+        menu: document.querySelector('[menu-overlay'),
+        navStudio: document.querySelector('[menu-btn-studio'),
+        navPortfolio: document.querySelector('[menu-btn-portfolio'),
+        navContacts: document.querySelector('[menu-btn-contacts'),
+
+    }
+
+    refs.openmenuBtn.addEventListener('click', togglemenu);
+    refs.closemenuBtn.addEventListener('click', togglemenu);
+    refs.navStudio.addEventListener('click', togglemenu);
+    refs.navPortfolio.addEventListener('click', closeModal);
+    refs.navContacts.addEventListener('click', togglemenu);
+
+    function togglemenu() {
+        refs.menu.classList.toggle('is-open');
+    }
+    function closeModal() {
+        refs.menu.classList.remove('is-open')
+    }
+
+})();
